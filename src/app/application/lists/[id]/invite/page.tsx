3a9107ee050a3fe24407e1807a70206fbb4fe167
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2023-05-13 16:12:05 -0300

    fix: remove semicolon

--- src/app/application/lists/[id]/invite/page.tsx
+++ src/app/application/lists/[id]/invite/page.tsx
@@ -25,7 +25,7 @@ export default function Invite({ params }: PageProps) {
       })
       setTimeout(() => {
         redirect('/application/lists')
-      }, 1000);
+      }, 1000)
     } catch (err) {
       if (isAxiosError(err)) {
         toast.error(err?.response?.data.message, {
