b566f3c2fbf257c0b698648e0e930ae75750ad50
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2023-05-13 00:00:55 -0300

    fix: improve overflow to main content

--- src/app/app/main.tsx
+++ src/app/app/main.tsx
@@ -6,7 +6,7 @@ import { SessionProvider } from 'next-auth/react'
 export const Main = ({ children }: any) => (
   <SessionProvider>
     <ListsProvider>
-      <main className="w-full h-full flex justify-center overflow-y-auto mb-[88px]">
+      <main className="w-full h-full flex justify-center overflow-x-hidden overflow-y-auto mb-[88px]">
         <div className="w-full max-w-[375px] h-full py-10">{children}</div>
       </main>
     </ListsProvider>
