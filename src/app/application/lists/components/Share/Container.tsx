934d1de5915300ca6087b4eed34aabf6618809d9
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2023-05-27 17:08:06 -0300

    fix: fix share modal

--- src/app/application/lists/components/Share/Container.tsx
+++ src/app/application/lists/components/Share/Container.tsx
@@ -101,163 +101,161 @@ export const Container = ({
     <Dialog.Root open={opened} onOpenChange={onOpenChange}>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/20" />
-        <Dialog.Content className="fixed flex flex-col items-center justify-center w-full h-full p-6">
-          <div className="flex flex-col w-full max-w-[512px] min-h-300px bg-zinc-50 rounded-xl p-6">
-            {isGetting ? (
-              <Spinner />
-            ) : (
-              <>
-                <header className="flex items-center mb-4">
-                  <h3 className="block flex-1 text-2xl font-semibold text-zinc-700 max-w-[90%] truncate">
-                    Compartilhe {`"${current.name}"`}
-                  </h3>
-                </header>
-                <div className="flex flex-col gap-4 mb-10">
-                  <form
-                    onSubmit={handleInviteMember}
-                    className="flex items-center gap-2"
+        <Dialog.Content className="fixed flex flex-col top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[calc(100%-3rem)] max-w-[512px] bg-zinc-50 p-6 rounded-xl min-h-[300px]">
+          {isGetting ? (
+            <Spinner />
+          ) : (
+            <>
+              <header className="flex items-center mb-4">
+                <h3 className="block flex-1 text-2xl font-semibold text-zinc-700 max-w-[90%] truncate">
+                  Compartilhe {`"${current.name}"`}
+                </h3>
+              </header>
+              <div className="flex flex-col gap-4 mb-10">
+                <form
+                  onSubmit={handleInviteMember}
+                  className="flex items-center gap-2"
+                >
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="Adicione membros pelo e-mail"
+                    className="w-full border-2 border-zinc-200 p-2 rounded outline-none placeholder:text-zinc-600 focus:border-red-500 focus:placeholder:text-red-400 peer transition"
+                  />
+                  <button
+                    type="submit"
+                    className="p-2 rounded bg-green-400 hover:bg-green-600 text-zinc-50 flex items-center justify-center transition"
                   >
-                    <input
-                      type="text"
-                      name="email"
-                      placeholder="Adicione membros pelo e-mail"
-                      className="w-full border-2 border-zinc-200 p-2 rounded outline-none placeholder:text-zinc-600 focus:border-red-500 focus:placeholder:text-red-400 peer transition"
-                    />
-                    <button
-                      type="submit"
-                      className="p-2 rounded bg-green-400 hover:bg-green-600 text-zinc-50 flex items-center justify-center transition"
+                    <Send />
+                  </button>
+                </form>
+                <div className="text-zinc-700">
+                  <h3 className="mb-4 text-lg font-semibold">Acesso geral</h3>
+                  <div className="flex items-center gap-4">
+                    <span
+                      className={clsx('p-2 rounded-full', {
+                        'bg-green-200': !current.restricted,
+                        'bg-zinc-200': current.restricted,
+                      })}
                     >
-                      <Send />
-                    </button>
-                  </form>
-                  <div className="text-zinc-700">
-                    <h3 className="mb-4 text-lg font-semibold">Acesso geral</h3>
-                    <div className="flex items-center gap-4">
-                      <span
-                        className={clsx('p-2 rounded-full', {
-                          'bg-green-200': !current.restricted,
-                          'bg-zinc-200': current.restricted,
-                        })}
-                      >
-                        {current.restricted ? (
-                          <Lock />
-                        ) : (
-                          <Globe className="stroke-green-800" />
-                        )}
-                      </span>
-                      <div>
-                        <DropdownMenu.Root>
-                          <DropdownMenu.Trigger asChild>
-                            <button className="flex items-center text-sm font-medium p-2 rounded hover:bg-zinc-200 gap-2 transition">
-                              {current.restricted
-                                ? 'Restrito'
-                                : 'Qualquer pessoa com o link'}
-                              <ChevronDown size={16} />
-                            </button>
-                          </DropdownMenu.Trigger>
-                          <DropdownMenu.Portal>
-                            <DropdownMenu.Content
-                              align="start"
-                              className={clsx([
-                                'bg-zinc-50 shadow-md rounded py-2 transition',
-                                'radix-side-bottom:animate-slideUpAndFade',
-                              ])}
+                      {current.restricted ? (
+                        <Lock />
+                      ) : (
+                        <Globe className="stroke-green-800" />
+                      )}
+                    </span>
+                    <div>
+                      <DropdownMenu.Root>
+                        <DropdownMenu.Trigger asChild>
+                          <button className="flex items-center text-sm font-medium p-2 rounded hover:bg-zinc-200 gap-2 transition">
+                            {current.restricted
+                              ? 'Restrito'
+                              : 'Qualquer pessoa com o link'}
+                            <ChevronDown size={16} />
+                          </button>
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Portal>
+                          <DropdownMenu.Content
+                            align="start"
+                            className={clsx([
+                              'bg-zinc-50 shadow-md rounded py-2 transition',
+                              'radix-side-bottom:animate-slideUpAndFade',
+                            ])}
+                          >
+                            <DropdownMenu.Item
+                              className="flex items-center hover:bg-zinc-200 px-4 cursor-pointer"
+                              onClick={() =>
+                                !current.restricted
+                                  ? handleChangeVisibility()
+                                  : () => {}
+                              }
                             >
-                              <DropdownMenu.Item
-                                className="flex items-center hover:bg-zinc-200 px-4 cursor-pointer"
-                                onClick={() =>
-                                  !current.restricted
-                                    ? handleChangeVisibility()
-                                    : () => {}
-                                }
+                              <span
+                                className={clsx(
+                                  'min-w-[24px] min-h-[24px] p-2 box-content',
+                                  {
+                                    'text-red-500': current.restricted,
+                                  },
+                                )}
                               >
-                                <span
-                                  className={clsx(
-                                    'min-w-[24px] min-h-[24px] p-2 box-content',
-                                    {
-                                      'text-red-500': current.restricted,
-                                    },
-                                  )}
-                                >
-                                  {current.restricted && <Check />}
-                                </span>
-                                Restrito
-                              </DropdownMenu.Item>
-                              <DropdownMenu.Item
-                                className="flex items-center hover:bg-zinc-200 px-4 cursor-pointer"
-                                onClick={() =>
-                                  current.restricted
-                                    ? handleChangeVisibility()
-                                    : () => {}
-                                }
+                                {current.restricted && <Check />}
+                              </span>
+                              Restrito
+                            </DropdownMenu.Item>
+                            <DropdownMenu.Item
+                              className="flex items-center hover:bg-zinc-200 px-4 cursor-pointer"
+                              onClick={() =>
+                                current.restricted
+                                  ? handleChangeVisibility()
+                                  : () => {}
+                              }
+                            >
+                              <span
+                                className={clsx(
+                                  'min-w-[24px] min-h-[24px] p-2 box-content',
+                                  {
+                                    'text-red-500': !current.restricted,
+                                  },
+                                )}
                               >
-                                <span
-                                  className={clsx(
-                                    'min-w-[24px] min-h-[24px] p-2 box-content',
-                                    {
-                                      'text-red-500': !current.restricted,
-                                    },
-                                  )}
-                                >
-                                  {!current.restricted && <Check />}
-                                </span>
-                                Qualquer pessoa com o link
-                              </DropdownMenu.Item>
-                            </DropdownMenu.Content>
-                          </DropdownMenu.Portal>
-                        </DropdownMenu.Root>
-                        <small className="p-2 block">
-                          {current.restricted
-                            ? 'Apenas as pessoas com acesso podem abrir com o link'
-                            : 'Qualquer pessoa com o link pode se tornar um membro'}
-                        </small>
-                      </div>
+                                {!current.restricted && <Check />}
+                              </span>
+                              Qualquer pessoa com o link
+                            </DropdownMenu.Item>
+                          </DropdownMenu.Content>
+                        </DropdownMenu.Portal>
+                      </DropdownMenu.Root>
+                      <small className="p-2 block">
+                        {current.restricted
+                          ? 'Apenas as pessoas com acesso podem abrir com o link'
+                          : 'Qualquer pessoa com o link pode se tornar um membro'}
+                      </small>
                     </div>
                   </div>
                 </div>
-                <footer className="flex items-center justify-between">
-                  <button
-                    className="flex items-center gap-2 px-3 py-2 rounded-3xl text-blue-500 border border-zinc-400 font-semibold transition"
-                    onClick={handleCopyLink}
-                  >
-                    <div className="w-[18px] h-[18px] relative">
-                      <Clipboard
-                        size={18}
-                        strokeWidth={3}
-                        style={{
-                          position: 'absolute',
-                          top: 0,
-                          left: 0,
-                          strokeDasharray: 60,
-                          strokeDashoffset: copied ? -60 : 0,
-                          transition: 'all 300ms ease-in-out',
-                        }}
-                      />
-                      <Check
-                        size={18}
-                        strokeWidth={3}
-                        style={{
-                          top: 0,
-                          left: 0,
-                          position: 'absolute',
-                          strokeDasharray: 60,
-                          strokeDashoffset: copied ? 0 : -60,
-                          transition: 'all 300ms ease-in-out',
-                        }}
-                      />
-                    </div>
-                    {!copied ? 'Copiar link' : 'Link copiado'}
-                  </button>
-                  <button
-                    className="bg-blue-500 px-3 py-2 rounded-3xl text-zinc-100"
-                    onClick={() => onOpenChange(false)}
-                  >
-                    Concluído
-                  </button>
-                </footer>
-              </>
-            )}
-          </div>
+              </div>
+              <footer className="flex items-center justify-between">
+                <button
+                  className="flex items-center gap-2 px-3 py-2 rounded-3xl text-blue-500 border border-zinc-400 font-semibold transition"
+                  onClick={handleCopyLink}
+                >
+                  <div className="w-[18px] h-[18px] relative">
+                    <Clipboard
+                      size={18}
+                      strokeWidth={3}
+                      style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        strokeDasharray: 60,
+                        strokeDashoffset: copied ? -60 : 0,
+                        transition: 'all 300ms ease-in-out',
+                      }}
+                    />
+                    <Check
+                      size={18}
+                      strokeWidth={3}
+                      style={{
+                        top: 0,
+                        left: 0,
+                        position: 'absolute',
+                        strokeDasharray: 60,
+                        strokeDashoffset: copied ? 0 : -60,
+                        transition: 'all 300ms ease-in-out',
+                      }}
+                    />
+                  </div>
+                  {!copied ? 'Copiar link' : 'Link copiado'}
+                </button>
+                <button
+                  className="bg-blue-500 px-3 py-2 rounded-3xl text-zinc-100"
+                  onClick={() => onOpenChange(false)}
+                >
+                  Concluído
+                </button>
+              </footer>
+            </>
+          )}
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
